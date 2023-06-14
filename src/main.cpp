3120f25e03b84fbcebbfc4ab3b7e4a0cdce38857
AbdelkrimBoucekine <boucekine.a@gmail.com>
2023-06-14 14:02:06 +0200

    led qui s'allume selon la valeur de turbidité

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <threadLvgl.h>
 #include "AnalogIn.h"
 #include <cstdio>
+#include "DigitalOut.h"
 
 ThreadLvgl threadLvgl(30);
 AnalogIn turbiditySensor(A0); // Capteur de turbidité connecté à la broche A0
@@ -11,6 +12,9 @@ lv_chart_series_t *ser1;
 bool isPaused = false;
 lv_obj_t *label;
 lv_obj_t *label_state;
+lv_obj_t *bar; // Nouvel objet pour la barre
+lv_obj_t *led1;
+lv_obj_t *led2;
 
 void update_turbidity_value() {
     while (1) {
@@ -44,8 +48,25 @@ void update_turbidity_value() {
             }
 
             lv_label_set_text(label_state, state);
-        }
 
+            // Mettre à jour la valeur de la barre
+            lv_bar_set_value(bar, turbidity, LV_ANIM_ON);
+
+            // Allumer ou éteindre la LED2 en fonction de la valeur de turbidité
+            if (turbidity > 50) {
+                lv_led_on(led2);
+            } else {
+                lv_led_off(led2);
+            }
+
+            // Allumer ou éteindre la LED1 en fonction de la valeur de turbidité
+            if (turbidity < 50) {
+                lv_led_on(led1);
+            } else {
+                lv_led_off(led1);
+            }
+
+        }
         ThisThread::sleep_for(500ms); // Attendre un court instant avant la prochaine lecture
     }
 }
@@ -61,6 +82,12 @@ static void btn_play_event_cb(lv_event_t *e) {
         isPaused = false;
     }
 }
+
+void toggle_leds() {
+    lv_led_toggle(led1);
+    lv_led_toggle(led2);
+}
+
 
 int main() {
     threadLvgl.lock();
@@ -92,7 +119,7 @@ int main() {
     // Créer le bouton Pause
     lv_obj_t *btn_pause = lv_btn_create(lv_scr_act());
     lv_obj_set_size(btn_pause, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
-    lv_obj_align(btn_pause, LV_ALIGN_BOTTOM_MID, -50, 0);
+    lv_obj_align(btn_pause, LV_ALIGN_LEFT_MID, 0, 50);
     lv_obj_add_event_cb(btn_pause, btn_pause_event_cb, LV_EVENT_CLICKED, NULL);
 
     // Créer le label pour le bouton Pause
@@ -102,7 +129,7 @@ int main() {
     // Créer le bouton Relancer
     lv_obj_t *btn_play = lv_btn_create(lv_scr_act());
     lv_obj_set_size(btn_play, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
-    lv_obj_align(btn_play, LV_ALIGN_BOTTOM_MID, 50, 0);
+    lv_obj_align(btn_play, LV_ALIGN_LEFT_MID, 0, -50);
     lv_obj_add_event_cb(btn_play, btn_play_event_cb, LV_EVENT_CLICKED, NULL);
 
     // Créer le label pour le bouton Relancer
@@ -111,22 +138,46 @@ int main() {
 
     // Créer le texte pour afficher la valeur du graphique
     label = lv_label_create(lv_scr_act());
-    lv_obj_align(label, LV_ALIGN_CENTER, 0, -75);
+    lv_obj_align(label, LV_ALIGN_CENTER, 50, 75);
     lv_label_set_text(label, "");
 
-    // Créer le texte pour afficher l'état de la valeur du capteur
+    // Créer le texte pour afficher "taux de turbidité"
+    lv_obj_t *label_title = lv_label_create(lv_scr_act());
+    lv_label_set_text(label_title, "Taux de turbidité: ");
+    lv_obj_align(label_title, LV_ALIGN_CENTER, -30, 75);
+
+    // Créer le texte pour afficher l'état du liquide
     label_state = lv_label_create(lv_scr_act());
-    lv_obj_align(label_state, LV_ALIGN_CENTER, 0, 75);
+    lv_obj_align(label_state, LV_ALIGN_CENTER, 0, 100);
     lv_label_set_text(label_state, "");
+
+    // Créer la barre
+    bar = lv_bar_create(lv_scr_act());
+    lv_obj_set_size(bar, 20, 200);
+    lv_obj_center(bar);
+    lv_bar_set_range(bar, 0, 100); // Définir la plage de la barre
+    lv_obj_align(bar, LV_ALIGN_CENTER, 115, 0);
+
+    // Créer les LEDs
+    led1 = lv_led_create(lv_scr_act());
+    lv_obj_align(led1, LV_ALIGN_BOTTOM_RIGHT, -10, -10);
+    lv_led_off(led1);
+    lv_led_set_color(led1, lv_palette_main(LV_PALETTE_RED));
+
+
+    led2 = lv_led_create(lv_scr_act());
+    lv_obj_align(led2, LV_ALIGN_BOTTOM_RIGHT, -50, -10);
+    lv_led_off(led2);
+    lv_led_set_color(led2, lv_palette_main(LV_PALETTE_BLUE));
+
 
     threadLvgl.unlock();
 
     printf("Démarrage du programme...\n");
-
     Thread updateThread;
     updateThread.start(update_turbidity_value);
 
-    // Créer un fragment vide pour afficher une page noire au lancement
+     // Créer un fragment vide pour afficher une page noire au lancement
     lv_obj_t *fragment = lv_obj_create(lv_scr_act());
     lv_obj_set_size(fragment, lv_disp_get_hor_res(NULL), lv_disp_get_ver_res(NULL));
     lv_obj_set_style_bg_opa(fragment, LV_OPA_COVER, 0);
@@ -140,20 +191,18 @@ int main() {
     lv_style_init(&style_welcome);
     lv_obj_add_style(label_welcome, &style_welcome, 0);
 
-    lv_style_set_text_font(&style_welcome, &lv_font_montserrat_48);  /*Set a larger font*/
-
+    lv_style_set_text_font(&style_welcome, &lv_font_montserrat_22);  // Set a larger font
 
     lv_obj_align(label_welcome, LV_ALIGN_CENTER, 0, 0);
-
-
 
     // Afficher la page noire pendant 5 secondes
     ThisThread::sleep_for(5s);
 
     // Supprimer le fragment vide
-    lv_obj_del(fragment);
+    lv_obj_del(fragment);   
 
+    // Créer une boucle d'événements LVGL
     while (1) {
-        ThisThread::sleep_for(2s); // Attendre quelques secondes avant la prochaine lecture
+        ThisThread::sleep_for(10ms);
     }
 }
