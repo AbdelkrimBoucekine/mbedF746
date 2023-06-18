4d73669662cb29bb11a3529fdcb2d586524f65df
AbdelkrimBoucekine <boucekine.a@gmail.com>
2023-06-18 14:33:15 +0200

    ajout message box lvgl (non fonctionnelle)

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,10 @@
 #include "AnalogIn.h"
 #include <cstdio>
 #include "DigitalOut.h"
+#include <ctime>
+#include <cstring>
+
+
 
 ThreadLvgl threadLvgl(30);
 AnalogIn turbiditySensor(A0); // Capteur de turbidité connecté à la broche A0
@@ -15,9 +19,23 @@ lv_obj_t *label_state;
 lv_obj_t *bar; // Nouvel objet pour la barre
 lv_obj_t *led1;
 lv_obj_t *led2;
+lv_obj_t *mbox; //Boîte de dialogue
 
+std::string get_current_time() {
+    // Obtenez l'heure actuelle en utilisant time() et localtime()
+    std::time_t now = std::time(nullptr);
+    std::tm* local_time = std::localtime(&now);
+
+    // Créez une chaîne de caractères au format HH:MM:SS
+    char time_str[9];  // 8 caractères pour HH:MM:SS + 1 caractère nul de fin
+    std::strftime(time_str, sizeof(time_str), "%H:%M:%S", local_time);
+
+    // Retournez la chaîne de caractères
+    return std::string(time_str);
+}
 
 void update_turbidity_value() {
+        const char* state = "État inconnu"; // Valeur par défaut
     while (1) {
         if (!isPaused) {
             // Lire la valeur du capteur de turbidité
@@ -38,18 +56,17 @@ void update_turbidity_value() {
             lv_label_set_text(label, value_str);
 
             // Mettre à jour l'état en fonction de la valeur du capteur
-            const char *state;
             if (turbidity >= 0 && turbidity <= 25) {
                 state = "L'eau est propre";
             } else if (turbidity > 25 && turbidity <= 50) {
-                state = "L'eau est légèrement propre";
+                state = "L'eau est legerement propre";
             } else if (turbidity > 50 && turbidity <= 75) {
-                state = "L'eau est légèrement sale";
+                state = "L'eau est legerement sale";
             } else if (turbidity > 75 && turbidity <= 100) {
-                state = "L'eau est très sale";
-            } else {
-                state = "État inconnu";
-            }
+                state = "L'eau est tres sale";
+            } //else if (turbidity > 50) {
+                //show_turbidity_message();
+           // }
 
             lv_label_set_text(label_state, state);
 
@@ -86,9 +103,25 @@ static void btn_play_event_cb(lv_event_t *e) {
     }
 }
 
-void toggle_leds() {
-    lv_led_toggle(led1);
-    lv_led_toggle(led2);
+void reset_chart() {
+    lv_chart_hide_series(chart, ser1, true); // Réinitialiser la série du graphique en supprimant toutes les données
+    lv_chart_refresh(chart); // Rafraîchir le graphique
+}
+
+// Définition de la fonction event_cb
+static void event_cb(lv_event_t *e) {
+    // Traitement des événements du message box
+    lv_obj_t * mbox1 = lv_event_get_current_target(e);
+    LV_LOG_USER("Button %s clicked", lv_msgbox_get_active_btn_text(obj));
+}
+
+// Définir la fonction lv_example_msgbox_1()
+void lv_example_msgbox_1(void) {
+    // ...
+    static const char * btns[] = {"Apply", "Close", ""};
+    lv_obj_t * mbox1 = lv_msgbox_create(NULL, "Attention", "This is a message box with two buttons.", btns, true);
+    lv_obj_add_event_cb(mbox1, event_cb, LV_EVENT_VALUE_CHANGED, NULL);
+    lv_obj_center(mbox1);
 }
 
 int main() {
@@ -109,7 +142,7 @@ int main() {
     // Créer un graphique
     chart = lv_chart_create(lv_scr_act());
     lv_obj_set_size(chart, 200, 150);
-    lv_obj_align(chart, LV_ALIGN_CENTER, 0, -25);
+    lv_obj_align(chart, LV_ALIGN_CENTER, 30, -30);
     lv_chart_set_type(chart, LV_CHART_TYPE_LINE);
 
     // Ajouter une série de données au graphique
@@ -121,7 +154,7 @@ int main() {
     // Créer le bouton Pause
     lv_obj_t *btn_pause = lv_btn_create(lv_scr_act());
     lv_obj_set_size(btn_pause, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
-    lv_obj_align(btn_pause, LV_ALIGN_BOTTOM_LEFT, 0, -5);
+    lv_obj_align(btn_pause, LV_ALIGN_BOTTOM_LEFT, 10, -5);
     lv_obj_add_event_cb(btn_pause, btn_pause_event_cb, LV_EVENT_CLICKED, NULL);
 
     // Créer le label pour le bouton Pause
@@ -131,12 +164,12 @@ int main() {
     // Créer le bouton Relancer
     lv_obj_t *btn_play = lv_btn_create(lv_scr_act());
     lv_obj_set_size(btn_play, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
-    lv_obj_align(btn_play, LV_ALIGN_BOTTOM_LEFT, 0, -50);
+    lv_obj_align(btn_play, LV_ALIGN_BOTTOM_LEFT, 10, -50);
     lv_obj_add_event_cb(btn_play, btn_play_event_cb, LV_EVENT_CLICKED, NULL);
 
-    // Créer le label pour le bouton Relancer
+    // Créer le label pour le bouton Reprendre
     lv_obj_t *label_play = lv_label_create(btn_play);
-    lv_label_set_text(label_play, "Relancer");
+    lv_label_set_text(label_play, "Reprendre");
 
     // Créer le texte pour afficher la valeur du graphique
     label = lv_label_create(lv_scr_act());
@@ -145,7 +178,7 @@ int main() {
 
     // Créer le texte pour afficher "taux de turbidité"
     lv_obj_t *label_title = lv_label_create(lv_scr_act());
-    lv_label_set_text(label_title, "Taux de turbidité: ");
+    lv_label_set_text(label_title, "Taux de turbidite: ");
     lv_obj_align(label_title, LV_ALIGN_CENTER, -30, 75);
 
     // Créer le texte pour afficher l'état du liquide
@@ -159,7 +192,7 @@ int main() {
     lv_obj_set_size(bar, 20, 200);
     lv_obj_center(bar);
     lv_bar_set_range(bar, 0, 100); // Définir la plage de la barre
-    lv_obj_align(bar, LV_ALIGN_CENTER, 115, 0);
+    lv_obj_align(bar, LV_ALIGN_CENTER, 130, 0);
 
     lv_style_set_bg_opa(&style_indic, LV_OPA_COVER);
     lv_style_set_bg_color(&style_indic, lv_palette_main(LV_PALETTE_RED));
@@ -176,6 +209,26 @@ int main() {
     lv_obj_align(led2, LV_ALIGN_BOTTOM_RIGHT, -50, -10);
     lv_led_off(led2);
     lv_led_set_color(led2, lv_palette_main(LV_PALETTE_BLUE));
+
+    // Créer le texte pour afficher l'heure
+    lv_obj_t *label_time = lv_label_create(lv_scr_act());
+    lv_obj_align(label_time, LV_ALIGN_TOP_RIGHT, -10, 10);
+    lv_label_set_text(label_time, get_current_time().c_str());
+
+    // Appeler lv_example_msgbox_1() pour afficher la boîte de dialogue
+    lv_example_msgbox_1();
+
+    lv_obj_t *btn_reset = lv_btn_create(lv_scr_act());
+    lv_obj_set_size(btn_reset, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
+    lv_obj_align(btn_reset, LV_ALIGN_BOTTOM_LEFT, 10, -95);
+    lv_obj_add_event_cb(btn_reset, [](lv_event_t *e) {
+        if (e->code == LV_EVENT_CLICKED) {
+            reset_chart(); // Utiliser la fonction reset_chart() pour réinitialiser le graphique
+        }
+    }, LV_EVENT_CLICKED, NULL);
+
+    lv_obj_t *label_reset = lv_label_create(btn_reset);
+    lv_label_set_text(label_reset, "Reinitialiser");
 
     threadLvgl.unlock();
 
@@ -197,18 +250,20 @@ int main() {
     lv_style_init(&style_welcome);
     lv_obj_add_style(label_welcome, &style_welcome, 0);
 
-    lv_style_set_text_font(&style_welcome, &lv_font_montserrat_22);  // Set a larger font
+    // Appliquer le style au label "Bienvenue"
+    lv_obj_add_style(label_welcome, &style_welcome, 0);
 
+    // Centrer le label "Bienvenue" sur l'écran
     lv_obj_align(label_welcome, LV_ALIGN_CENTER, 0, 0);
 
-    // Afficher la page noire pendant 5 secondes
-    ThisThread::sleep_for(5s);
+    // Afficher le fragment pendant 3 secondes
+    ThisThread::sleep_for(500ms);
 
-    // Supprimer le fragment vide
+    // Supprimer le fragment après l'affichage
     lv_obj_del(fragment);
 
-    // Créer une boucle d'événements LVGL
     while (1) {
-        ThisThread::sleep_for(10ms);
+        lv_task_handler();
+        ThisThread::sleep_for(5ms);
     }
 }
